991b8c296eaf45a6871f98d96d75d6f3087145ef
nishus266 <nishant.sharma.cs.2016@miet.ac.in>
2019-01-17 20:46:38 +0530

    starting

---
+++ java/com/mrigindia/mrigindia/MainActivity.java
@@ -0,0 +1,24 @@
+package com.mrigindia.mrigindia;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private static int time_out= 5000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, advertise.class);
+                startActivity(i);
+                finish();
+            }
+        }, time_out);
+
+    }
+}
---
+++ java/com/mrigindia/mrigindia/advertise.java
@@ -0,0 +1,13 @@
+package com.mrigindia.mrigindia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class advertise extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advertise);
+    }
+}
